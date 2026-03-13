7d54ceb42ae39d5509efcfdd403759d75aea96ca
agnesl-adobe <agnesl@adobe.com>
2026-03-13 15:18:27 +0000

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -186,9 +186,10 @@ async function loadPage() {
   loadDelayed();
 }
 
+loadPage();
+
 (async function loadDa() {
   if (!new URL(window.location.href).searchParams.get('dapreview')) return;
   import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
 }());
 
-loadPage();
